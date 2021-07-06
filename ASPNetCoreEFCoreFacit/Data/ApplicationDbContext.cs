6f7c7e243301a2d7a4c48443e7fc81e473e91297
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-07-06 10:17:37 +0200

    Add project files.

---
+++ ASPNetCoreEFCoreFacit/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNetCoreEFCoreFacit.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options)
+        {
+            
+        }
+
+        public DbSet<Bil> Bilar { get; set; }
+    }
+}
